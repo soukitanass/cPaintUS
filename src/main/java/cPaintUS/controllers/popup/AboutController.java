be779e37623627b63e8ddf12c38bd833147a8195
Marc-Eric Pelletier <medaragon@hotmail.com>
2019-01-27 15:45:46 -0500

    Added something in the about popup.

--- src/main/java/cPaintUS/controllers/popup/AboutController.java
+++ src/main/java/cPaintUS/controllers/popup/AboutController.java
@@ -1,5 +1,38 @@
 package cPaintUS.controllers.popup;
 
-public class AboutController {
 
+import javafx.fxml.FXML;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class AboutController {
+	private Stage newDialog;
+	
+	@FXML
+	private Label description;
+	@FXML
+	private Button closeBtn;
+	
+	public void setNewDialog(Stage stage) {
+		this.newDialog = stage;
+	}
+	
+	@FXML
+	private void initialize() {
+		this.description.setPadding(new Insets(10, 20, 10, 20));
+		this.description.setWrapText(true);
+		this.description.setText("Created by cPaintUS team members:\nJean Bilski\nCorentin Chatelin\nNoemie Landry-Boisvert\nSoukaina Nassib\nMarc-Eric Pelletier");
+		this.closeBtn.setAlignment(Pos.BOTTOM_CENTER);
+		VBox.setMargin(this.closeBtn, new Insets(10));
+	}
+	
+	@FXML
+	private void handleCloseClick() {
+		newDialog.close();
+	}
+	
 }
