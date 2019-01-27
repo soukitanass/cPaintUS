d2ab1501af069cf91fd37fdaa23a69a3ecadcc79
Unknown <chtln.c@hotmail.fr>
2019-01-27 13:18:07 -0500

    Corrected TopPane

--- src/main/java/cPaintUS/controllers/TopPaneController.java
+++ src/main/java/cPaintUS/controllers/TopPaneController.java
@@ -38,6 +38,7 @@ public class TopPaneController {
 	public TopPaneController() {
 		snapshotSingleton = SnapshotSingleton.getInstance();
 		shapesDict = ShapesDict.getInstance();
+		centralCloseController = CentralCloseController.getInstance();
 	}
 
 	@FXML
