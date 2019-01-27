eb04c54863c3a7db416fc33ae28f93535ea86799
Noémie Landry-Boisvert <noemie.landry-boisvert@usherbrooke.ca>
2019-01-27 17:11:18 -0500

    Added attributes

--- src/main/java/cPaintUS/controllers/LeftPaneController.java
+++ src/main/java/cPaintUS/controllers/LeftPaneController.java
@@ -4,15 +4,26 @@ import cPaintUS.models.DrawSettings;
 import cPaintUS.models.LineWidth;
 import cPaintUS.models.observable.IObserver;
 import cPaintUS.models.observable.ObservableList;
+import cPaintUS.models.shapes.Ellipse;
+import cPaintUS.models.shapes.Heart;
+import cPaintUS.models.shapes.Pokeball;
+import cPaintUS.models.shapes.Rectangle;
 import cPaintUS.models.shapes.Shape;
 import cPaintUS.models.shapes.ShapeType;
 import cPaintUS.models.shapes.ShapesDict;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ColorPicker;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.ToolBar;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 
 public class LeftPaneController implements IObserver {
 	
@@ -31,6 +42,26 @@ public class LeftPaneController implements IObserver {
 	private Button eraseAllBtn;
 	@FXML
 	private ListView<Shape> shapeList;
+	@FXML
+	private ToolBar attributes;
+	@FXML
+	private Label attributesLabel;
+	@FXML
+	private ComboBox<String> editLineWidth;
+	@FXML
+	private ColorPicker editFillColor;
+	@FXML
+	private ColorPicker editStrokeColor;
+	@FXML
+	private Spinner<Integer> editX;
+	@FXML
+	private Spinner<Integer> editY;
+	@FXML
+	private Spinner<Integer> editWidth;
+	@FXML
+	private Spinner<Integer> editHeight;
+	@FXML
+	private Spinner<Integer> rotate;
 	
 	public void setRoot(RootController rootController) {
 	}
@@ -53,17 +84,67 @@ public class LeftPaneController implements IObserver {
 		lineWidth.setValue(LineWidth.getInstance().getDefaultString());
 		
 		// Set ColorPickers default value to black
+		fillColor.setDisable(true);
 		fillColor.setValue(Color.BLACK);
 		strokeColor.setValue(Color.BLACK);
+		
+		editLineWidth.getItems().setAll(LineWidth.getInstance().getStrings());
+		attributes.setVisible(false);
+		
+		// Event listener when shape is selected
+		shapeList.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Shape>() {
+			@Override
+			public void changed(ObservableValue<? extends Shape> observable, Shape oldShape, Shape newShape) {
+				if (newShape == null) {
+					attributes.setVisible(false);
+					return;
+				}
+				attributes.setVisible(true);
+				attributesLabel.setText(newShape.getShapeId() + " Attributes:");
+				attributesLabel.setFont(Font.font("System", FontWeight.BOLD, 12));
+				editLineWidth.setValue(newShape.getLineWidth() + "px");			
+				editFillColor.setDisable(false);
+
+				switch (newShape.getShapeType()) {
+					case Ellipse: 
+						editFillColor.setValue(Color.web(((Ellipse) newShape).getFillColor()));
+						break;
+					case Heart:
+						editFillColor.setValue(Color.web(((Heart) newShape).getFillColor()));
+						break;
+					case Pokeball:
+						editFillColor.setValue(Color.web(((Pokeball) newShape).getFillColor()));
+						break;
+					case Rectangle:
+						editFillColor.setValue(Color.web(((Rectangle) newShape).getFillColor()));
+						break;
+					default:
+						editFillColor.setDisable(true);
+						break;
+				}
+				
+				editStrokeColor.setValue(Color.web(newShape.getStrokeColor()));
+				editX.getValueFactory().setValue((int) newShape.getX());
+				editY.getValueFactory().setValue((int) newShape.getY());
+				editWidth.getValueFactory().setValue((int) newShape.getWidth());
+				editHeight.getValueFactory().setValue((int) newShape.getHeight());
+			}
+		});
 	}
 	
 	@FXML
 	private void handleChangeShape() {
 		drawSettings.setShape(shape.getValue());
+		
+		if (shape.getValue() == ShapeType.Line) {
+			fillColor.setDisable(true);
+		} else {
+			fillColor.setDisable(false);
+		}
 	}
 	
 	@FXML
-	private void handleChangeBrushSize() {
+	private void handleChangeLineWidth() {
 		// Extract the integer in the string
 		String widthStr = lineWidth.getValue().replaceAll("[^0-9]", "");
 		int newWidth = Integer.parseInt(widthStr);
@@ -84,6 +165,25 @@ public class LeftPaneController implements IObserver {
 	@FXML
 	private void handleEraseAllClick() {
 		SnapshotSingleton.getInstance().eraseAll();
+	}
+	
+	@FXML
+	private void handleEditLineWidth() {
+		// Extract the integer in the string
+		String widthStr = lineWidth.getValue().replaceAll("[^0-9]", "");
+		int newWidth = Integer.parseInt(widthStr);
+		
+		//
+	}
+	
+	@FXML
+	private void handleEditFillColor() {
+		//
+	}
+	
+	@FXML
+	private void handleEditStrokeColor() {
+		//
 	}
 
 	@Override
