297484b39348756aa9f5ad1eed39cabbdd300519
soukainanassib <30549229+soukainanassib@users.noreply.github.com>
2019-02-04 15:18:49 -0500

    fix test imports

--- src/test/java/cPaintUS/models/ShapeFactoryTest.java
+++ src/test/java/cPaintUS/models/ShapeFactoryTest.java
@@ -1,7 +1,7 @@
 package cPaintUS.models;
 
-import static org.junit.Assert.assertSame;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
