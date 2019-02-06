3f1efbfd390a52434e78f4b3d3461fc7047d2d40
soukainanassib <30549229+soukainanassib@users.noreply.github.com>
2019-02-06 10:30:07 -0500

    fix xml file manager test

--- src/test/java/cpaintus/models/FileManagerStrategyTest.java
+++ src/test/java/cpaintus/models/FileManagerStrategyTest.java
@@ -1,36 +1,55 @@
 package cpaintus.models;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import cpaintus.models.savestrategy.FileManagerStrategy;
+import cpaintus.models.savestrategy.XMLStrategy;
+import cpaintus.models.shapes.ShapeFactory;
+import cpaintus.models.shapes.ShapeType;
+import cpaintus.models.shapes.ShapesDictionnary;
+
 class FileManagerStrategyTest {
-	/*
-	 * private FileManagerStrategy fileManagerStrategy; private final String
-	 * XML_FILE_TO_LOAD_PATH = "xmlFileLoadTest.xml"; private final String
-	 * XML_FILE_TO_SAVE_PATH = "xmlFileSaveTest.xml"; private ShapesDict shapesDict;
-	 * private ShapeFactory shapeFactory;
-	 * 
-	 * @Rule public TemporaryFolder temporaryFolder = new TemporaryFolder();
-	 * 
-	 * @BeforeEach void setUp() throws Exception { shapesDict =
-	 * ShapesDict.getInstance(); shapeFactory = ShapeFactory.getInstance();
-	 * temporaryFolder.create(); }
-	 * 
-	 * @Test void testLoadXML() { fileManagerStrategy = new XMLStrategy();
-	 * fileManagerStrategy.load(getClass().getClassLoader().getResource(
-	 * XML_FILE_TO_LOAD_PATH).getPath());
-	 * assertFalse(shapesDict.getShapesList().isEmpty()); }
-	 * 
-	 * @Test void testSaveXML() { fileManagerStrategy = new XMLStrategy();
-	 * shapesDict.clearShapes();
-	 * shapesDict.addShape(shapeFactory.getShape(ShapeType.Rectangle, true, 3333,
-	 * 11, 1, 10, 100, 1, "#00000", "#00000","hah"));
-	 * fileManagerStrategy.save(resolvePath(XML_FILE_TO_SAVE_PATH));
-	 * shapesDict.clearShapes();
-	 * fileManagerStrategy.load(resolvePath(XML_FILE_TO_SAVE_PATH));
-	 * assertFalse(shapesDict.getShapesList().isEmpty());
-	 * 
-	 * }
-	 * 
-	 * private String resolvePath(String folder) { return
-	 * temporaryFolder.getRoot().toPath().resolve(folder).toString(); }
-	 */
+
+	private FileManagerStrategy fileManagerStrategy;
+	private final String XML_FILE_TO_LOAD_PATH = "xmlFileLoadTest.xml";
+	private final String XML_FILE_TO_SAVE_PATH = "xmlFileSaveTest.xml";
+	private ShapesDictionnary shapesDict;
+	private ShapeFactory shapeFactory;
+
+	public TemporaryFolderExtension temporaryFolder = new TemporaryFolderExtension();
+
+	@BeforeEach
+	void setUp() throws Exception {
+		shapesDict = ShapesDictionnary.getInstance();
+		shapeFactory = ShapeFactory.getInstance();
+		temporaryFolder.beforeEach(null);
+	}
+
+	@Test
+	void testLoadXML() {
+		fileManagerStrategy = new XMLStrategy();
+		fileManagerStrategy.load(getClass().getClassLoader().getResource(XML_FILE_TO_LOAD_PATH).getPath());
+		assertFalse(shapesDict.getShapesList().isEmpty());
+	}
+
+	@Test
+	void testSaveXML() {
+		fileManagerStrategy = new XMLStrategy();
+		shapesDict.clearShapes();
+		shapesDict.addShape(shapeFactory.getShape(ShapeType.RECTANGLE, true, 3333, 11, 1, 10, 100, 1, 2, 7, "#00000",
+				"#00000", null, "test"));
+		fileManagerStrategy.save(resolvePath(XML_FILE_TO_SAVE_PATH));
+		shapesDict.clearShapes();
+		fileManagerStrategy.load(resolvePath(XML_FILE_TO_SAVE_PATH));
+		assertFalse(shapesDict.getShapesList().isEmpty());
+
+	}
+
+	private String resolvePath(String folder) {
+		return temporaryFolder.getRoot().toPath().resolve(folder).toString();
+	}
 
 }
