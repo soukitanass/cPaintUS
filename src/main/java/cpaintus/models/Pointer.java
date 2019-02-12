617e9062d32cacff0c89daf025b5f67048d83596
Jean Bilski <jean.bilski@gmail.com>
2019-02-12 16:14:49 -0500

    Magnetic grid

--- src/main/java/cpaintus/models/Pointer.java
+++ src/main/java/cpaintus/models/Pointer.java
@@ -7,9 +7,13 @@ import cpaintus.models.observable.ObservableList;
 public class Pointer extends Observable<IObserver>{
 
 	private Point cursorPoint;
+	private Boolean isGridActivated;
+	private double gridStep;
 	
 	private Pointer() {
 		cursorPoint = new Point();
+		isGridActivated = true;
+		gridStep = 50;
 	}
 
 	private static class SingletonHelper {
@@ -21,8 +25,14 @@ public class Pointer extends Observable<IObserver>{
 	}
 	
 	public void setCursorPoint(double x, double y) {
-		cursorPoint.setX(x);
-		cursorPoint.setY(y);
+		if(isGridActivated) {
+			cursorPoint.setX(Math.round((x/gridStep)+ 0.5) * gridStep);
+			cursorPoint.setY(Math.round((y/gridStep)+ 0.5) * gridStep);
+			System.out.println(cursorPoint.getX() + " - " + x);
+		} else {
+			cursorPoint.setX(x);
+			cursorPoint.setY(y);			
+		}
 		this.notifyAllObservers();
 	}
 
