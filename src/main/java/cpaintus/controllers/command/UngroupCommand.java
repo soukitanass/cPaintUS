90b88cd197817296a916b9a2b1ae80b2baa6e71a
Noémie Landry-Boisvert <noemie.landry-boisvert@usherbrooke.ca>
2019-02-18 01:10:20 -0500

    ..

--- src/main/java/cpaintus/controllers/command/UngroupCommand.java
+++ src/main/java/cpaintus/controllers/command/UngroupCommand.java
@@ -1,5 +1,8 @@
 package cpaintus.controllers.command;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import cpaintus.models.BoundingBox;
 import cpaintus.models.composite.ShapesGroup;
 import cpaintus.models.shapes.Shape;
@@ -8,6 +11,7 @@ import cpaintus.models.shapes.ShapesDictionnary;
 public class UngroupCommand extends Command {
 
 	private ShapesGroup shapesGroup;
+	private List<ShapesGroup> parents;
 	private ShapesDictionnary shapesDict;
 	private BoundingBox boundingBox;
 	
@@ -25,18 +29,27 @@ public class UngroupCommand extends Command {
 	@Override
 	public void execute() {
 		shapesDict.removeShape(shapesGroup);
+		
+		parents = new ArrayList<ShapesGroup>();
+		shapesDict.findParents(shapesGroup, shapesDict.getShapesList(), parents);
+		
 		boundingBox.setVisible(false);
 	}
 
 	@Override
 	public void undo() {
 		for (Shape shape : shapesGroup.getShapes()) {
-			if (!shapesGroup.getShapes().isEmpty()) {
-				shapesDict.addShape(shapesGroup);
-				shapesDict.removeShape(shape, false);
-			}
+			shapesDict.removeShape(shape, false);
 		}
-
+		
+		if (this.parents.size() != 0) {
+			for (ShapesGroup parent : this.parents) {
+				parent.add(shapesGroup);
+				shapesDict.addShape(parent); // To trigger event in left pane
+			}
+		} else {
+			shapesDict.addShape(shapesGroup);
+		}
 	}
 
 }
