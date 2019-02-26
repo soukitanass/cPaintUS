b24b883da32ce838b4f7a745c5fc9b353c79a2d3
Jean Bilski <jean.bilski@gmail.com>
2019-02-25 22:11:00 -0500

    ctrl+shift+f

--- src/main/java/cpaintus/controllers/CenterContainerController.java
+++ src/main/java/cpaintus/controllers/CenterContainerController.java
@@ -1,6 +1,5 @@
 package cpaintus.controllers;
 
-
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.control.ScrollPane;
@@ -11,38 +10,40 @@ public class CenterContainerController {
 
 	@FXML
 	private Canvas leftRuler;
-	
+
 	@FXML
 	private Canvas topRuler;
-	
+
 	@FXML
 	private GridPane gridPane;
-	
+
 	@FXML
 	private ScrollPane leftScroll;
-	
+
 	@FXML
 	private ScrollPane topScroll;
-	
+
 	@FXML
 	public void initialize() {
 		gridPane.setStyle("-fx-focus-color: #C8C7C5;");
-		
-		gridPane.widthProperty().addListener((obs,oldValue,newValue)->{
-			topRuler.setWidth(newValue.doubleValue()-50);
+
+		gridPane.widthProperty().addListener((obs, oldValue, newValue) -> {
+			topRuler.setWidth(newValue.doubleValue() - 50);
 			drawRulers();
 		});
-		gridPane.heightProperty().addListener((obs,oldValue,newValue)->{
-			leftRuler.setHeight(newValue.doubleValue()-50);
+		gridPane.heightProperty().addListener((obs, oldValue, newValue) -> {
+			leftRuler.setHeight(newValue.doubleValue() - 50);
 			drawRulers();
 		});
-		drawRulers();		
+		drawRulers();
 	}
-	
+
 	private void drawRulers() {
 		leftRuler.getGraphicsContext2D().setFill(Color.PURPLE);
-		leftRuler.getGraphicsContext2D().fillRect(0, 0, leftRuler.getWidth(), leftRuler.getHeight());;
+		leftRuler.getGraphicsContext2D().fillRect(0, 0, leftRuler.getWidth(), leftRuler.getHeight());
+		;
 		topRuler.getGraphicsContext2D().setFill(Color.PURPLE);
-		topRuler.getGraphicsContext2D().fillRect(0, 0, topRuler.getWidth(), topRuler.getHeight());;
+		topRuler.getGraphicsContext2D().fillRect(0, 0, topRuler.getWidth(), topRuler.getHeight());
+		;
 	}
 }
