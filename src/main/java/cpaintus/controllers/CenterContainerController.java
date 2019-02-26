d3281ddbb217f469fbfe3b69719942a330553261
Jean Bilski <jean.bilski@gmail.com>
2019-02-25 20:38:17 -0500

    two ruler to rule them all

---
+++ src/main/java/cpaintus/controllers/CenterContainerController.java
@@ -0,0 +1,46 @@
+package cpaintus.controllers;
+
+
+import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+
+public class CenterContainerController {
+
+	@FXML
+	private Canvas leftRuler;
+	
+	@FXML
+	private Canvas topRuler;
+	
+	@FXML
+	private GridPane gridPane;
+	
+	@FXML
+	private ScrollPane leftScroll;
+	
+	@FXML
+	private ScrollPane topScroll;
+	
+	@FXML
+	public void initialize() {
+		gridPane.widthProperty().addListener((obs,oldValue,newValue)->{
+			topRuler.setWidth(newValue.doubleValue()-50);
+			drawRulers();
+		});
+		gridPane.heightProperty().addListener((obs,oldValue,newValue)->{
+			leftRuler.setHeight(newValue.doubleValue()-50);
+			drawRulers();
+		});
+		drawRulers();		
+	}
+	
+	private void drawRulers() {
+		leftRuler.getGraphicsContext2D().setFill(Color.PURPLE);
+		leftRuler.getGraphicsContext2D().fillRect(0, 0, leftRuler.getWidth(), leftRuler.getHeight());;
+		topRuler.getGraphicsContext2D().setFill(Color.PURPLE);
+		topRuler.getGraphicsContext2D().fillRect(0, 0, topRuler.getWidth(), topRuler.getHeight());;
+	}
+}
--- src/main/java/cpaintus/controllers/RootController.java
+++ src/main/java/cpaintus/controllers/RootController.java
@@ -1,32 +1,7 @@
 package cpaintus.controllers;
 
-import javafx.fxml.FXML;
 
 public class RootController {
 
-	@FXML
-	private LeftPaneController leftPaneController;
-	@FXML
-	private CenterPaneController centerPaneController;
-	@FXML
-	private BottomPaneController bottomPaneController;
-	@FXML
-	private TopPaneController topPaneController;
-
-	public LeftPaneController getLeftPaneController() {
-		return leftPaneController;
-	}
-
-	public CenterPaneController getCenterPaneController() {
-		return centerPaneController;
-	}
-
-	public BottomPaneController getBottomPaneController() {
-		return bottomPaneController;
-	}
-
-	public TopPaneController getTopPaneController() {
-		return topPaneController;
-	}
 
 }
